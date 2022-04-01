60ec496102d46e509915d2b34a4d323855fd008c
Davaskye DeBique <Davaskye@gmail.com>
2022-03-31 23:50:06 -0400

    Finishing touch

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,6 +20,9 @@
               <RouterLink to="/" class="nav-link active">Home</RouterLink>
             </li>
             <li class="nav-item">
+              <RouterLink class="nav-link" to="/news">News</RouterLink>
+            </li>
+            <li class="nav-item">
               <RouterLink class="nav-link" to="/about">About</RouterLink>
             </li>
           </ul>
